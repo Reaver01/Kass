95628b06c8f87e031e2ecdeaefe0f5da96b22797
Reaver01 <reaver_01@gmx.com>
2017-03-12 11:24:10 -0500

    add similar searching

--- src/modules/commands/compendium.rb
+++ src/modules/commands/compendium.rb
@@ -7,36 +7,46 @@ module Bot
       extend Discordrb::Commands::CommandContainer
       command(
         :comp,
-        description: 'Search something within the Hyrule Compendium. Must be spelled correctly. Case insensitive.',
+        description: 'Search something within the Hyrule Compendium. Must be spelled correctly. C' \
+          'ase insensitive.',
         usage: 'comp mat/loc item to be searched'
       ) do |event, option, *search|
         search = search.join(' ').downcase
         Database::CommandLog.resolve_name('Compendium').log
         if option == 'mat'
-          found_item = Database::Material.find(name: search)
-          if found_item.nil?
+          similar = Database::Material.where(Sequel.ilike(:name, "#{search}%"))
+          if similar.count.zero?
             'Item not found.'
+          elsif similar.count == 1
+            event.channel.send_embed 'I found this in the Hyrule Compendium:', similar[0].info_embed
           else
-            event.channel.send_embed 'I found this in the Hyrule Compendium:', found_item.info_embed
+            event << 'Perhaps one of these is what you are looking for:'
+            similar.each { |m| event << m.name.titleize }
+            nil
           end
         end
         if option == 'loc'
-          found_location = Database::Location.find(name: search)
-          if found_location.nil?
+          similar = Database::Location.where(Sequel.ilike(:name, "#{search}%"))
+          if similar.count.zero?
             'Location not found.'
-          else
+          elsif similar.count == 1
             embed = Discordrb::Webhooks::Embed.new
-            embed.title = found_location.name.titleize
-            embed.thumbnail = { url: found_location.image }
+            embed.title = similar[0].name.titleize
+            embed.thumbnail = { url: similar[0].image }
             embed.description = ''
-            unless found_location.materials.length.zero?
+            unless similar[0].materials.length.zero?
               embed.description += "**Materials**\n"
-              found_location.materials.each do |m|
+              similar[0].materials.each do |m|
                 embed.description += "\##{m.item_id} - #{m.name.titleize}\n"
               end
             end
             embed.timestamp = Time.now
             event.channel.send_embed 'I found this in the Hyrule Compendium:', embed
+          else
+            event << 'Perhaps one of these is what you are looking for:'
+            similar.each { |m| event << m.name.titleize }
+            nil
+          end
           end
         end
       end
