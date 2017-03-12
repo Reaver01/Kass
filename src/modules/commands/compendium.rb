0fa38dd8c185970d6d1b0363b5760220d820778f
Reaver01 <reaver_01@gmx.com>
2017-03-12 11:51:30 -0500

    creature lookup

--- src/modules/commands/compendium.rb
+++ src/modules/commands/compendium.rb
@@ -13,7 +13,19 @@ module Bot
       ) do |event, option, *search|
         search = search.join(' ').downcase
         Database::CommandLog.resolve_name('Compendium').log
-        if option == 'mat'
+        if %w(creatures creature creat).any? { |o| option.include?(o) }
+          similar = Database::Creatures.where(Sequel.ilike(:name, "%#{search}%"))
+          if similar.count.zero?
+            'Creature not found.'
+          elsif similar.count == 1
+            event.channel.send_embed 'I found this in the Hyrule Compendium:',
+                                     similar.all[0].info_embed
+          elsif similar.count > 1
+            event << 'Perhaps one of these is what you are looking for:'
+            similar.each { |m| event << m.name.titleize }
+            nil
+          end
+        elsif %w(materials material mats mat).any? { |o| option.include?(o) }
           similar = Database::Material.where(Sequel.ilike(:name, "%#{search}%"))
           if similar.count.zero?
             'Item not found.'
@@ -25,8 +37,7 @@ module Bot
             similar.each { |m| event << m.name.titleize }
             nil
           end
-        end
-        if option == 'loc'
+        elsif %w(locations location locs loc).any? { |o| option.include?(o) }
           similar = Database::Location.where(Sequel.ilike(:name, "%#{search}%"))
           if similar.count.zero?
             'Location not found.'
@@ -35,6 +46,12 @@ module Bot
             embed.title = similar.all[0].name.titleize
             embed.thumbnail = { url: similar.all[0].image }
             embed.description = ''
+            unless similar.all[0].creatures.length.zero?
+              embed.description += "**Creatures**\n"
+              similar.all[0].creatures.each do |m|
+                embed.description += "\##{m.item_id} - #{m.name.titleize}\n"
+              end
+            end
             unless similar.all[0].materials.length.zero?
               embed.description += "**Materials**\n"
               similar.all[0].materials.each do |m|
--- src/modules/database/compendium_creature.rb
+++ src/modules/database/compendium_creature.rb
@@ -9,6 +9,29 @@ module Bot
       def self.resolve_id(id)
         find_or_create(item_id: id)
       end
+
+      def info_embed
+        embed = Discordrb::Webhooks::Embed.new
+        embed.title = "\##{item_id} - #{name.titleize}"
+        embed.thumbnail = { url: image }
+        embed.description = ''
+        embed.description += "**Description**\n#{description}\n" unless description.nil?
+        unless locations.length.zero?
+          embed.description += "**Common Locations**\n"
+          locations.each { |l| embed.description += "#{l.name.titleize}\n" }
+        end
+        unless materials.length.zero?
+          embed.description += "**Recoverable Materials**\n"
+          materials.each { |m| embed.description += "#{m.name.titleize}\n" }
+        end
+        embed.description += "**Hearts Recovered**\n#{hearts.round(2)}\n" unless hearts.zero?
+        unless effects.length.zero?
+          embed.description += "**Cooking Effects**\n"
+          effects.each { |e| embed.description += "#{e.name.titleize}\n" }
+        end
+        embed.timestamp = Time.now
+        embed
+      end
     end
   end
 end
